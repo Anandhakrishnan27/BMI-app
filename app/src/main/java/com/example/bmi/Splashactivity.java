724ecf7a0b86dfe0b782d972f12aef28eac0aab4
Anandhakrishnan27 <anandhshiva27@gmail.com>
2021-06-13 12:18:26 +0530

    first commit

---
+++ app/src/main/java/com/example/bmi/Splashactivity.java
@@ -0,0 +1,25 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashactivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3500);
+    }
+}
